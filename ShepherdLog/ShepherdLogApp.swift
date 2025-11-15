00554126443409a8635ee21eccd88b425444a829
esplusnin <esplusnin94@yandex.ru>
2025-11-15 13:51:20 +0300

    Initial Commit

---
+++ ShepherdLog/ShepherdLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShepherdLogApp.swift
+//  ShepherdLog
+//
+//  Created by Евгений on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShepherdLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
